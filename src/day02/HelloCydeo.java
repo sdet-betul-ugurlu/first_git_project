534697cfdf3a10507e01562d0ed965ae1392838d
Ugurlubet <betldzc@gmail.com>
2026-02-14 02:10:24 +0300

    commit1

---
+++ src/day02/HelloCydeo.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class HelloCydeo {
+    public static void main(String[] args) {
+        System.out.println("Hello Cydeo");
+             System.out.println("Hello World");
+         System.out.println("Hello Side");
+
+        
+    }
+}
