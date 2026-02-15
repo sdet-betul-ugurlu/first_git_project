8effcddb0b8121a345bb2005efedf8b4f4ba6c62
Betül UĞURLU <sdet.betul.ugurlu@gmail.com>
2026-02-15 21:54:25 +0300

    Update hello.java

--- src/day02/hello.java
+++ src/day02/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("*  *");
         System.out.println(" * ");
+        System.out.println(" sdet ");
+        System.out.println(" betül ");
     }
 }
